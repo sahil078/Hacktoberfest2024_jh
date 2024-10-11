255583e8c692437c0a8e53cb7c42332ba5356497
rithvik <basanirithvikreddy2@gmail.com>
2024-10-11 16:20:25 +0530

    added Youtube Clone to HTML,CSS,JS Projects

--- 5 Projects Using html css js/Project3/script.js
+++
@@ -1,7 +0,0 @@
-let cur = document.querySelector(".cursor");
-let main = document.querySelector("#main");
-
-main.addEventListener("mousemove" , (details)=>{
-    cur.style.top=details.y+"px";
-    cur.style.left = details.x + "px";
-})
