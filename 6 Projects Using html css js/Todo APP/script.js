ab5d6e301ed9487ee00a34406dafcb3cfbe19518
Sumits-Cosmos <uvcosmos2@gmail.com>
2024-10-15 22:21:19 +0530

    first commit for hacktoberfest2024

---
+++ 6 Projects Using html css js/Todo APP/script.js
@@ -0,0 +1,37 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+function addTask(){
+    if(inputBox.value === ''){
+        alert("you must write something!");
+        console.log("item clicked")
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+            span.innerHTML = "\u00d7";
+            li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+    saveData();
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+showTask();
