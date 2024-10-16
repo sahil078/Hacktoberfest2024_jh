d611b3ae3933de7e41342346a06b1412ecac776f
Riya Khare <riya.khare.1106@gmail.com>
2024-10-16 11:24:25 +0530

    Added rock-paper-scissors game and its README file

---
+++ Python/python-mini-games/Rock-paper-scissors.py
@@ -0,0 +1,48 @@
+import random
+
+
+user_wins = 0
+computer_wins = 0
+
+options = ['rock', 'paper', 'scissors']
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == 'q':
+        break
+    
+    if user_input not in ['rock', 'paper', 'scissors']:
+         continue
+     
+    random_number = random.randint(0,2)
+    #  rock: 0, paper: 1, scissors: 2
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick)
+    
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You win")
+        user_wins += 1
+        continue
+    
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You win")
+        user_wins += 1
+        continue
+    
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You win")
+        user_wins += 1
+        continue
+    
+    elif user_input == computer_pick:
+        print("You both picked the same")
+        continue
+
+    else:
+        print("You lost")
+        computer_wins += 1
+     
+print("You won", user_wins, "times")
+print("The computer won", computer_wins, "times")    
+print("Goodbye!")
+         
